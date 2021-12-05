90192496013ee336c3bf3725bd7d91e2cf3da276
fabianzielinski <fabian.zielinski@o2.pl>
2021-12-05 21:28:42 +0100

    Update portfolio date

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -11,10 +11,11 @@ const portfoliolist = [
     id: 1,
     src: portfolio,
     alt: "Portfolio_Photo",
-    hrefdemo: "https://github.com/fabianzielinski/Portfolio",
+    hrefdemo: "https://fabianzielinski.github.io/Portfolio/",
     hrefgit: "https://github.com/fabianzielinski/Portfolio",
-    title: "Portfolio",
+    title: "One Page Portfolio",
     techinfo: "Website, Javascript, HTML, CSS, RWD",
+    info: "Simple one page Portfolio.",
   },
   {
     id: 2,
@@ -24,6 +25,7 @@ const portfoliolist = [
     hrefgit: "https://github.com/fabianzielinski/online-electronics-store",
     title: "Final Project Online Electronics Store",
     techinfo: "Website, React, Javascript, HTML, CSS, Bootstrap, RWD",
+    info: "A website for an online electronics shop in React. ",
   },
   {
     id: 3,
@@ -33,6 +35,7 @@ const portfoliolist = [
     hrefgit: "https://github.com/fabianzielinski/Cezar",
     title: "Cesar's code",
     techinfo: "Website, Javascript, HTML, CSS, RWD",
+    info: "A simple website that uses Caesar code to encrypt and decrypt text messages.",
   },
   {
     id: 4,
@@ -42,6 +45,7 @@ const portfoliolist = [
     hrefgit: "https://github.com/fabianzielinski/one-page-fw-jasna",
     title: "One Page Farma Wiatrowa Jasna",
     techinfo: "Website, HTML, SCSS, RWD",
+    info: "Simple one page about Wind Farm Jasna.",
   },
   {
     id: 5,
@@ -51,6 +55,7 @@ const portfoliolist = [
     hrefgit: "https://github.com/fabianzielinski/space-landing-page",
     title: "Space-landing-page",
     techinfo: "Website, HTML, SCSS, REACT",
+    info: "Simple one page in React .",
   },
 ];
 
@@ -87,8 +92,9 @@ const Portfolio = () => {
               <button className="project_button github">GitHub</button>
             </a>
             <div className="project_content">
-              <h4>{project.title}</h4>
-              <p>{project.techinfo}</p>
+              <h3>{project.title}</h3>
+              <h4>{project.techinfo}</h4>
+              <p>{project.info}</p>
             </div>
           </div>
         ))}
